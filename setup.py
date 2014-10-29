079d1baeb44cb3402101e7368ac53ded590aecbe
Louis Thibault <louist@gmail.com>
2014-10-30 00:37:51 +0100

    PIP-installable release

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 
 setup(
     name='txCelery',
-    version='0.0.2 alpha',
+    version='1.0.0',
     author='Sentimens Research Group, LLC',
     author_email='contact@sentimens.com',
     packages=['txcelery'],
     include_package_data=True,
-    install_requires=['Twisted>=11.1.0', 'Celery>=3.1.16'],
+    install_requires=['Twisted>=11.0.0', 'Celery>=3.0.0'],
     url='https://github.com/SentimensRG/txCelery',
     license='MIT',
     description=('Celery for Twisted:  manage Celery tasks from twisted'
