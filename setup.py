d77b13e95ae085ba582c7f0add8fa3dd32f13899
Louis Thibault <l.thibault@sentimens.com>
2014-10-30 00:17:25 +0100

    Fixed automatic versioning

--- setup.py
+++ setup.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-from txcelery import __version__
-
 setup(
     name='txCelery',
-    version=__version__,
+    version='0.0.2 alpha',
     author='Sentimens Research Group, LLC',
     author_email='contact@sentimens.com',
     packages=['txcelery'],
     include_package_data=True,
     install_requires=['Twisted>=11.1.0', 'Celery>=3.1.16'],
-    url='https://github.com/SentimensRG/TwistedCelery',
+    url='https://github.com/SentimensRG/txCelery',
     license='MIT',
     description=('Celery for Twisted:  manage Celery tasks from twisted'
                  'using the Deferred API'),
